5c202ccd7d0b4ce2b8f233de2369505efb806fa9
vamsi panchada <vamsivs58@gmail.com>
2023-10-17 12:42:33 +0530

    two sets corrected

--- py/Two Sets.py
+++ py/Two Sets.py
@@ -1,30 +1,19 @@
 n = int(input())
-if n%4==0 or (n+1)%4==0:
+s = (n+1)*n//2
+if s%2==0:
     print('YES')
-    if n%2==0:
-        print(n//2)
-        for i in range(1, n, 2):
-            print(i, end=' ')
-        print(n//2)
-        for i in range(2, n, 2):
-            print(i, end=' ')
-    else:
-        if n%8==0:
-            print(n//2+1)
-            for i in range(1, n, 2):
-                print(i, end=' ')
-            print(n)
-            print(n//2)
-            for i in range(2, n, 2):
-                print(i, end=' ')
+    t = s//2
+    l, r = [], []
+    for i in range(n, 0, -1):
+        if i<=t:
+            l.append(i)
+            t-=i
         else:
-            print(n//2)
-            for i in range(1, n, 2):
-                print(i, end=' ')
-            print()
-            print(n//2+1)
-            for i in range(2, n, 2):
-                print(i, end=' ')
-            print(n)
+            r.append(i)
+    print(len(l))
+    print(*l)
+    print(len(r))
+    print(*r)
+    
 else:
     print('NO')
