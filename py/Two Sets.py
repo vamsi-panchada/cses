1b077f5eb0257601df118250a0b0fc413cc08a42
Panchada Vamsi <panchada@crayondata.com>
2023-10-17 12:29:14 +0530

    two sets

---
+++ py/Two Sets.py
@@ -0,0 +1,27 @@
+n = int(input())
+if n%4==0 or (n+1)%4==0:
+    if n%2==0:
+        print(n//2)
+        for i in range(1, n, 2):
+            print(i, end=' ')
+        print(n//2)
+        for i in range(2, n, 2):
+            print(i, end=' ')
+    else:
+        if n%8==0:
+            print(n//2+1)
+            for i in range(1, n, 2):
+                print(i, end=' ')
+            print(n)
+            print(n//2)
+            for i in range(2, n, 2):
+                print(i, end=' ')
+        else:
+            print(n//2)
+            for i in range(1, n, 2):
+                print(i, end=' ')
+            print()
+            print(n//2+1)
+            for i in range(2, n, 2):
+                print(i, end=' ')
+            print(n)
