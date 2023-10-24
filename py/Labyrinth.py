999629f9d13e41d263cc7b23bfffb00f622328e2
vamsi panchada <vamsivs58@gmail.com>
2023-10-24 21:28:04 +0530

    new commit

--- py/Labyrinth.py
+++ py/Labyrinth.py
@@ -1,4 +1,20 @@
+def path(i, j, n, m, grid, ans, dir):
+    if i<0 or j<0 or i>=n or j>=m:
+        return
+    if grid[i][j]=='#':
+        return
+    else:
+        
+def solve(i, j, n, m, grid):
+    ans = []
+    if i-1>=0:
+        p
+
 n, m = map(int, input().split())
-l = []
+grid = []
 for _ in range(n):
-    l.append(list(input()))
+    grid.append(list(input()))
+for i in range(n):
+    for j in range(m):
+        if grid[i][j]=='A':
+            res = solve(i, j, n, m, grid)
